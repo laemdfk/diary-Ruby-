e2d5bbab7c283355b138c9697467d9afe2818fa3
laemdfk <kappa021618@icloud.com>
2022-08-24 17:08:06 +0000

    [Add]新規投稿機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,42 @@
 class PostsController < ApplicationController
   def new
+    @post_new = Post.new
   end
-  
-  def creaet
+
+  def create
+     @post_new = Post.new(post_params)
+    # @post_new = Post.new(params[:id])　→→これだとnilが渡る(照合するidがないから？)
+     if @post_new.save
+      redirect_to post_path(@post_new.id)
+      # @post_new.idとすることで、新規に与えられたidをもとに詳細ページへ飛べる
+     end
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+
   end
-  
+
   def update
+
   end
-  
+
   def destroy
+
   end
-  
-  
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:body,:post_image)
+  end
+
 end
