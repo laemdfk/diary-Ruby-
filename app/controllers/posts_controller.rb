35a3a5c459a2b516ec5070ca01aca3254837e80f
laemdfk <kappa021618@icloud.com>
2022-08-26 14:44:21 +0000

    [Add]投稿編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,11 +21,21 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
 
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    
+     if @post.update(post_params)
+        flash[:notice]="You have updated successfully."
+        redirect_to post_path(@post.id)
+       
+     else
+        render "edit"
+     end
+     
   end
 
   def destroy
