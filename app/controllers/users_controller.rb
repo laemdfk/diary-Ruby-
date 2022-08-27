cea3499a1303653224dddc9b11d9523600197575
laemdfk <kappa021618@icloud.com>
2022-08-27 16:34:01 +0000

    [Add]プロフィール編集機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+     @user = User.find(params[:id])
   end
-  
-  def index
+
+  def update
+    @user = User.find(params[:id])
+
+     if @user.update(user_params)
+        flash[:notice]="You have updated successfully."
+        redirect_to user_path(@user.id)
+
+     else
+        render "edit"
+     end
+
   end
-  
-  
+
+
+ private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
